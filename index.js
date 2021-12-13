2ed7b6e87d9de9af765df605dea9404cb9d0fc81
rvquichimbo <ronal1923@gmail.com>
2021-12-13 13:02:09 -0500

    programar tesis

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const config = require('config')
 
 app.get('/', (req, res)=>{
-    res.send('CI /CD Devops tesis, Tesis Bienvenidad a mi aplicación Ing. Karla well !')
+    res.send('CI /CD Devops , Tesis Bienvenidad a mi aplicación Ing. Karla well !')
 })
 
 app.get('/status', (req, Sres)=>{
